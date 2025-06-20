f2f631948f17afcb4f93b9533603f3ceda718593
karthiksivasai <kkarthik3162@gmail.com>
2025-06-20 15:14:43 +0530

    Rename and run: Exercise 2 - E-commerce Platform Search Function.java in Algorithms_Data Structures

--- week1/Design Patterns and Principles/Exercise 2 - Implementing the Factory Method Pattern.java
+++ week1/Design Patterns and Principles/Exercise 2 - Implementing the Factory Method Pattern.java
@@ -49,19 +49,19 @@ class ExcelDocumentFactory extends DocumentFactory {
     }
 }
 
-
-
-public class DocumentFactoryTest {
+class DocumentFactoryTest {
     public static void main(String[] args) {
-
+        // Create a Word document using its factory
         DocumentFactory wordFactory = new WordDocumentFactory();
         Document wordDoc = wordFactory.createDocument();
         wordDoc.open();
 
+        // Create a PDF document using its factory
         DocumentFactory pdfFactory = new PdfDocumentFactory();
         Document pdfDoc = pdfFactory.createDocument();
         pdfDoc.open();
 
+        // Create an Excel document using its factory
         DocumentFactory excelFactory = new ExcelDocumentFactory();
         Document excelDoc = excelFactory.createDocument();
         excelDoc.open();
